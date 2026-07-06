6823abe91d5a163ae21370df15123e45020823d1
Pranam Pagi <pranampagi@gmail.com>
2026-07-06 10:17:00 +0530

    Initial scaffold: Go module, Task struct, JSON storage, CLI dispatch

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+const usage = `Usage: task-cli <command> [arguments]
+
+Commands:
+  add <description>               Add a new task
+  update <id> <description>       Update a task description
+  delete <id>                     Delete a task
+  mark-in-progress <id>           Mark a task as in progress
+  mark-done <id>                  Mark a task as done
+  list [done|todo|in-progress]    List tasks
+`
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Print(usage)
+		os.Exit(0)
+	}
+
+	command := os.Args[1]
+
+	switch command {
+	case "add":
+		handleAdd(os.Args[2:])
+	case "update":
+		handleUpdate(os.Args[2:])
+	case "delete":
+		handleDelete(os.Args[2:])
+	case "mark-in-progress":
+		handleMark(os.Args[2:], StatusInProgress)
+	case "mark-done":
+		handleMark(os.Args[2:], StatusDone)
+	case "list":
+		handleList(os.Args[2:])
+	default:
+		fmt.Fprintf(os.Stderr, "Unknown command: %s\n\n%s", command, usage)
+		os.Exit(1)
+	}
+}
+
+func handleAdd(args []string) {
+	fmt.Println("add: not yet implemented")
+}
+
+func handleUpdate(args []string) {
+	fmt.Println("update: not yet implemented")
+}
+
+func handleDelete(args []string) {
+	fmt.Println("delete: not yet implemented")
+}
+
+func handleMark(args []string, status string) {
+	fmt.Println("mark: not yet implemented")
+}
+
+func handleList(args []string) {
+	fmt.Println("list: not yet implemented")
+}
---
+++ storage.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+)
+
+const taskFile = "tasks.json"
+
+func LoadTasks() ([]Task, error) {
+	data, err := os.ReadFile(taskFile)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return []Task{}, nil
+		}
+		return nil, err
+	}
+
+	var tasks []Task
+	if err := json.Unmarshal(data, &tasks); err != nil {
+		return nil, err
+	}
+	return tasks, nil
+}
+
+func SaveTasks(tasks []Task) error {
+	data, err := json.MarshalIndent(tasks, "", "  ")
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(taskFile, data, 0644)
+}
